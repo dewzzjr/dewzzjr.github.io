d7269a022814614b0669e689c9a80b202faa5adf
Dewangga P P <dewzzpro@gmail.com>
2018-07-15 22:00:51 +0700

    First commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function _calculateAge(birthday) { // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+var age = _calculateAge(new Date("11-15-1995"))
+console.log(age)
+$("#age").html(age)
